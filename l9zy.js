521b6c12484fcefe41aadf20fe38c0fc3556c1a1
Luddo <ludo@neuviemepage.com>
2016-02-13 13:19:08 +0100

    add index.html for sample use and js hint run

--- l9zy.js
+++ l9zy.js
@@ -8,152 +8,132 @@
 
 var l9zy = (function() {
 
- 	"use strict";
+            "use strict";
 
- 	var module = {};
- 	var scrollY,
- 		options = {
- 			ratioElementInViewPort : 0
- 		},
- 		items = [],
- 		getViewportH = window.innerHeight,
- 		lazys = document.querySelectorAll('.lazy');
+            var scrollY,
+                options = {
+                    ratioElementInViewPort: 0
+                },
+                items = [],
+                getViewportH = window.innerHeight,
+                lazys = document.querySelectorAll('.lazy');
 
- 	var throttle = function(callback, limit) {
- 		var wait = false;
- 		return function() {
- 			if (!wait) {
- 				callback.call();
- 				wait = true;
- 				setTimeout(function() {
- 					wait = false;
- 				}, limit);
- 			}
- 		}
- 	};
+            var throttle = function(callback, limit) {
+                var wait = false;
+                return function() {
+                    if (!wait) {
+                        callback.call();
+                        wait = true;
+                        setTimeout(function() {
+                            wait = false;
+                        }, limit);
+                    }
+                };
+            };
 
- 	var scrollY = function() {
+            var inViewport = function(el, h) {
 
- 		return window.pageYOffset || document.documentElement.scrollTop;
+                var elH = el.elH,
+                    viewed = scrollY + getViewportH,
+                    elTop = el.elTop,
+                    _h = h || 0;
 
- 	};
+                return (elTop + (elH * _h)) <= viewed;
 
- 	function getViewportH() {
+            };
 
- 		return window.innerHeight;
+            var loadCallback = function(el) {
 
- 	};
+                return function() {
 
- 	function getOffset(el) {
+                    el.appendChild(this);
+                    requestAnimationFrame((function() {
+                        el.classList.add('loaded');
+                    }).bind(el));
 
- 		return $(el).offset();
- 	};
+                };
 
- 	function inViewport(el, h) {
+            };
 
- 		var elH 	 = el.elH,
- 			viewed 	 = scrollY + getViewportH,
- 			elTop 	 = el.elTop,
- 			elBottom = el.elBottom,
- 			_h 		 = h || 0;
+            var render = function() {
 
- 		return (elTop + (elH * _h)) <= viewed;
+                for (var i = 0; i < items.length; i++) {
 
- 	};
+                    var item = items[i];
 
- 	var render = function() {
+                    //if element lazy don't contain loaded class do the lazy thing
+                    if ((!item.el.classList.contains('loaded')) && (inViewport(item, options.ratioElementInViewPort)) && (!item.full)) {
 
- 		for (var i = 0; i < items.length; i++) {
+                        var imgSrc = item.el.getAttribute('data-src');
+                        var loader = document.createElement('span');
+                        loader.classList.add('spinner');
+                        item.el.appendChild(loader);
 
- 			var item = items[i];
- 			
- 			//if element lazy don't contain loaded class do the lazy thing
- 			if ( (!item.el.classList.contains('loaded')) 
- 				&& (inViewport(item, options.ratioElementInViewPort))
- 				&& (!item.full)) {
+                        item.full = true;
+                        var img = document.createElement('img');
 
- 				var imgSrc = item.el.getAttribute('data-src');
- 				var loader = document.createElement('span');
- 				loader.classList.add('spinner');
- 				item.el.appendChild(loader); 
- 				
- 				item.full = true;
- 				var img = document.createElement('img');
+                        img.onload = (loadCallback)(item.el);
 
- 				img.onload = (function(el) {
+                        img.src = imgSrc;
 
- 					return function() {
+                    }
 
- 						el.appendChild(this);
+                }
 
- 						requestAnimationFrame((function() {
- 							el.classList.add('loaded');
- 						}).bind(item.el));
+            };
 
- 					};
+            var resizeHandler = function() {
 
- 				})(item.el);
+                getViewportH = window.innerHeight;
+                items.length = 0;
 
- 				img.src = imgSrc;
+                for (var i = 0; i < lazys.length; i++) {
 
- 			}
+                    var el = lazys[i];
+                    if (!el.classList.contains("loaded")) {
+                            var elH = el.offsetHeight,
+                                elTop = el.getBoundingClientRect().top;
 
- 		}
+                            items.push({
+                                el: el,
+                                elTop: elTop,
+                                elH: elH
+                            });
+                        }
+                    }
 
- 	};
+                };
 
- 	var resizeHandler = function() {
+                var scrollHandler = function() {
 
- 		getViewportH = window.innerHeight;
- 		items.length = 0;
+                    scrollY = window.pageYOffset;
+                    requestAnimationFrame(function() {
+                        render();
+                    });
 
- 		for (var i = 0; i < lazys.length; i++) {
- 			var el    	 = lazys[i],
- 				elH   	 = el.offsetHeight,
- 				elTop 	 = el.getBoundingClientRect().top,
- 				elBottom = elTop + elH;
+                };
 
- 			items.push({
- 				el 		 : el,
- 				elTop 	 : elTop,
- 				elBottom : elBottom,
- 				elH 	 : elH
- 			});
- 		}
+                return {
 
+                    elements: function(els) {
+                        if (!els) {
+                            lazys = document.querySelectorAll('.lazy');
+                        } else {
+                            lazys = els;
+                        }
+                        resizeHandler();
+                        scrollHandler();
+                    },
 
- 	};
+                    init: function(_options) {
+                        options = _options || {};
+                        window.addEventListener("resize", throttle(resizeHandler, 100));
+                        window.addEventListener("scroll", throttle(scrollHandler, 100));
+                        resizeHandler();
+                        scrollHandler();
 
- 	var scrollHandler = function() {
+                    }
 
- 		scrollY = window.pageYOffset;
- 		requestAnimationFrame(function() {
- 			render();
- 		});
+                };
 
- 	};
-
- 	return {
-
- 		elements: function(els) {
- 			if(!els) {
- 				lazys = document.querySelectorAll('.lazy');
- 			} else {
- 				lazys = els
- 			}
- 			resizeHandler();
- 			scrollHandler();
- 		},
-
- 		init: function(_options) {
- 			options = _options;
- 			window.addEventListener("resize", throttle(resizeHandler,300));
- 			window.addEventListener("scroll", throttle(scrollHandler,300));
- 			resizeHandler();
- 			scrollHandler();
-
- 		}
-
- 	};
-
- })();
+            })();
